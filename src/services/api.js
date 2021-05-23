540fb0bdc2b22b3f67e54275688d5d21f616e789
Emerson Melo Martins <emerson25xd@gmail.com>
2021-05-22 21:16:59 -0300

    Added api on order detail page

--- src/services/api.js
+++ src/services/api.js
@@ -1,24 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   // baseURL: 'https://localhost:44361/api' // local
-  baseURL: 'http://api.emersonmelomartins.dev.br/api' // cloud
-  
-})
-
+  baseURL: "http://api.emersonmelomartins.dev.br/api", // cloud
+});
 
 api.interceptors.request.use(async (config) => {
   const storagedUser = localStorage.getItem("@Seuphone::user");
 
-  if(storagedUser !== null) {
+  if (storagedUser !== null) {
     const { token } = JSON.parse(localStorage.getItem("@Seuphone::user"));
-    
+
     config.headers.Authorization = `Bearer ${token}`;
   }
-  
+
   return config;
 });
-
-
 
 export default api;
