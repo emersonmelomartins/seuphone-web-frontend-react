a9b4a4da02487563054db5ebdc3f03ea275d36be
Emerson Melo Martins <emerson25xd@gmail.com>
2021-06-10 13:48:22 -0300

    Remoção de panel para correção

--- src/pages/panel/OrdersTab/EditOrderForm/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const EditOrderContainer = styled.div`
-  div form {
-    border-radius: 10px !important;
-    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.175);
-  }
-`;
